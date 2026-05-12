09a1cabf3601385c725ae9a9b0dfdb972c2b2c13
nohurincorporation <nohurincorporation@gmail.com>
2026-05-12 10:26:56 -0600

    fix(api): register auth guards in protected modules

--- apps/api/src/modules/engagement/engagement.module.ts
+++ apps/api/src/modules/engagement/engagement.module.ts
@@ -1,7 +1,8 @@
 import { Module } from "@nestjs/common";
 import { AuthModule } from "../auth/auth.module";
+import { MockAuthGuard, PermissionGuard } from "../auth/guards";
 import { EngagementController } from "./engagement.controller";
 import { EngagementService } from "./engagement.service";
 
-@Module({ imports: [AuthModule], controllers: [EngagementController], providers: [EngagementService] })
+@Module({ imports: [AuthModule], controllers: [EngagementController], providers: [EngagementService, MockAuthGuard, PermissionGuard] })
 export class EngagementModule {}
